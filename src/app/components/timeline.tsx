15aedfe98482ce2e9fa72cceae71c1632147b0b3
BoxBop <tohverrobert@gmail.com>
2025-01-29 19:50:31 +0200

    Added content, with better layout

--- src/app/components/timeline.tsx
+++ src/app/components/timeline.tsx
@@ -1,118 +1,100 @@
 const personalTimeline = [
 	{
-		year: "1984",
-		title: "First Macintosh computer",
+		year: "07.2023 — 06.2024",
+		title: "Military Service — Estonian Defence Forces",
 		description: `
-		The Apple Macintosh—later rebranded as the Macintosh 128K—is the
-		  original Apple Macintosh personal computer. It played a pivotal role
-		  in establishing desktop publishing as a general office function. The
-		  motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were
-		  housed in a beige case with integrated carrying handle; it came with a
-		  keyboard and single-button mouse.
+		Conducted regular monitoring and optimization of network infrastructure and servers to maintain system reliability and performance, particularly under variable and challenging field conditions.
+        Implemented stringent security protocols to protect sensitive military data and communications channels, actively managing access controls and encryption standards to prevent unauthorized access and data breaches.
+        Provided hands-on assistance to end-users, troubleshooting and resolving issues related to hardware, software, and communication devices, ensuring minimal downtime and high operational efficiency.
 		`,
 		done: true,
 	},
 	{
-		year: "1998",
-		title: "iMac",
+		year: "06.2021 — 12.2022",
+		title: "Back-End Developer — Pipedrive OÜ",
 		description: `
-		  iMac is a family of all-in-one Mac desktop computers designed and
-		  built by Apple Inc. It has been the primary part of Apple's consumer
-		  desktop offerings since its debut in August 1998, and has evolved
-		  through seven distinct forms
+			Integrated front-end and back-end components through APIs, focusing on the activities section of the CRM platform, using JavaScript and TypeScript.
+        	Conducted frequent code reviews, collaborating via Git to manage version control and contribute constructive feedback to the development team.
+        	Led multiple projects, overseeing task prioritization and ensuring timely completion by coordinating between developers and product managers.
+        	Maintained detailed records of IT incidents and system performance to facilitate efficient system upgrades, repairs, and audits.
 		`,
 		done: true,
 	},
 	{
-		year: "2001",
-		title: "iPod",
+		year: "10.2019 — 05.2021",
+		title: "Junior Back-End Developer — Pipedrive OÜ",
 		description: `
-		  The iPod is a discontinued series of portable media players and
-		  multi-purpose mobile devices designed and marketed by Apple Inc. The
-		  first version was released on October 23, 2001, about 8+1⁄2 months
-		  after the Macintosh version of iTunes was released. Apple sold an
-		  estimated 450 million iPod products as of 2022. Apple discontinued the
-		  iPod product line on May 10, 2022. At over 20 years, the iPod brand is
-		  the oldest to be discontinued by Apple
+			Started as a school intern, later transitioned to a full-time Junior Back-End Developer, contributing to core CRM platform features.
+        	Assisted in code quality improvements, bug fixes, and cross-functional integrations, working closely with back-end and front-end teams.
 		`,
 		done: true,
 	},
 	{
-		year: "2007",
-		title: "iPhone",
+		year: "2017 — 2020",
+		title: "Junior Software Engineer — Tallinn Polytechnic School",
 		description: `
-		  iPhone is a line of smartphones produced by Apple Inc. that use
-		  Apple's own iOS mobile operating system. The first-generation iPhone
-		  was announced by then-Apple CEO Steve Jobs on January 9, 2007. Since
-		  then, Apple has annually released new iPhone models and iOS updates.
-		  As of November 1, 2018, more than 2.2 billion iPhones had been sold.
-		  As of 2022, the iPhone accounts for 15.6% of global smartphone market
-		  share
+		  Graduated with honours. Alongside studies completed a year long internship at Pipedrive OÜ as a Junior Back-End Developer.
 		`,
-		done: false,
-	},
-	{
-		year: "2015",
-		title: "Apple Watch",
-		description: `
-		  The Apple Watch is a line of smartwatches produced by Apple Inc. It
-		  incorporates fitness tracking, health-oriented capabilities, and
-		  wireless telecommunication, and integrates with iOS and other Apple
-		  products and services
-		`,
-		done: false,
-	},
+		done: true,
+	}
 ];
 
 export default function Timeline() {
 	return (
-		<ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical max-w-7xl m-auto px-8">
-			{personalTimeline.map((item, index) => (
-				<li>
-					{(item.done || personalTimeline[index - 1].done) &&
-					index != 0 ? (
-						<hr
-							className={
-								item.done || personalTimeline[index - 1].done
-									? "bg-primary"
-									: ""
-							}
-						/>
-					) : (
-						""
-					)}
-					<div className="timeline-middle">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							className="h-5 w-5"
-						>
-							<path
-								fillRule="evenodd"
-								d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-								clipRule="evenodd"
+		<div>
+			<ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical max-w-7xl m-auto px-8">
+				{personalTimeline.map((item, index) => (
+					<li>
+						{(item.done || personalTimeline[index - 1].done) &&
+						index != 0 ? (
+							<hr
+								className={
+									item.done ||
+									personalTimeline[index - 1].done
+										? "bg-primary"
+										: ""
+								}
 							/>
-						</svg>
-					</div>
-					<div
-						className={
-							(index % 2 == 0
-								? "timeline-start md:text-end"
-								: "timeline-end md:text-start") + " mb-10"
-						}
-					>
-						<time className="font-mono italic">{item.year}</time>
-						<div className="text-lg font-black">{item.title}</div>
-						{item.description}
-					</div>
-					{index != personalTimeline.length - 1 ? (
-						<hr className={item.done ? "bg-primary" : ""} />
-					) : (
-						""
-					)}
-				</li>
-			))}
-		</ul>
+						) : (
+							""
+						)}
+						<div className="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								className="h-5 w-5"
+							>
+								<path
+									fillRule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clipRule="evenodd"
+								/>
+							</svg>
+						</div>
+						<div
+							className={
+								(index % 2 == 0
+									? "timeline-start md:text-end"
+									: "timeline-end md:text-start") + " mb-10"
+							}
+						>
+							<time className="font-mono italic">
+								{item.year}
+							</time>
+							<div className="text-lg font-black">
+								{item.title}
+							</div>
+							{item.description}
+						</div>
+						{index != personalTimeline.length - 1 ? (
+							<hr className={item.done ? "bg-primary" : ""} />
+						) : (
+							""
+						)}
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,57 +2,129 @@ import Image from "next/image";
 import Timeline from "./components/timeline";
 
 export default function Home() {
-  return (
-    <div className="">
-      <div className="navbar bg-base-300">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Link</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="bg-base-100 rounded-t-none p-2">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
-      </div>
+	return (
+		<div className="">
+			<div className="navbar bg-base-300">
+				<div className="flex-1">
+					<a className="btn btn-ghost text-xl">daisyUI</a>
+				</div>
+				<div className="flex-none">
+					<ul className="menu menu-horizontal px-1">
+						<li>
+							<a>Link</a>
+						</li>
+						<li>
+							<details>
+								<summary>Parent</summary>
+								<ul className="bg-base-100 rounded-t-none p-2">
+									<li>
+										<a>Link 1</a>
+									</li>
+									<li>
+										<a>Link 2</a>
+									</li>
+								</ul>
+							</details>
+						</li>
+					</ul>
+				</div>
+			</div>
 
-      <div>
-        <h1 className="text-5xl font-bold text-center">Box Office News!</h1>
-      </div>
-      <div className="hero bg-base-200 min-h-80 bg-transparent">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <Image className="rounded-xl" src={"/pfp.jpg"} alt="Profile picture" width="200" height="200"/>
-          <div className="max-w-3xl">
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
-        </div>
-      </div>
-      <Timeline/>
-    </div>
-  );
+			{/* <div>
+				<h1 className="text-5xl font-bold text-center">
+					Box Office News!
+				</h1>
+			</div> */}
+			<div className="card bg-base-300 rounded-box max-w-5xl mx-auto p-8 my-8">
+				<div className="flex">
+					<div className="flex-grow max-w-3xl text-center m-auto px-6">
+						<h1 className="text-5xl font-bold">Roberts Pauls Tohver</h1>
+						<p className="py-6">
+						I am an IT professional with a strong foundation in creating and enhancing digital solutions.
+						With hands-on experience in IT support, I am drawn to complex challenges, initiating innovation,
+						and data analysis aimed at improving processes. I have a keen interest in the latest technologies and their potential,
+						along with a desire to continually develop my skills to create valuable solutions that drive company growth.
+						I am confident in my ability to adapt and work in a team not only helps achieve goals but also fosters positive
+						relationships with colleagues and partners.
+						</p>
+					</div>
+					<div className="flex-none flex-column px-2">
+						<Image
+							className="rounded-xl"
+							src={"/pfp.jpg"}
+							alt="Profile picture"
+							width="200"
+							height="200"
+						/>
+						{/* <div className="avatar">
+						<div className="w-64 rounded">
+							<img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+						</div>
+					</div> */}
+						<ul className="menu bg-base-200 rounded-box w-m mt-4">
+							<li>
+								<a>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										className="h-7 w-7"
+										fill="none"
+										viewBox="0 0 32 32"
+										stroke="currentColor"
+									>
+										<path
+											// strokeLinecap="round"
+											// strokeLinejoin="round"
+											// strokeWidth="2"
+											d="M26.111,3H5.889c-1.595,0-2.889,1.293-2.889,2.889V26.111c0,1.595,1.293,2.889,2.889,2.889H26.111c1.595,0,2.889-1.293,2.889-2.889V5.889c0-1.595-1.293-2.889-2.889-2.889ZM10.861,25.389h-3.877V12.87h3.877v12.519Zm-1.957-14.158c-1.267,0-2.293-1.034-2.293-2.31s1.026-2.31,2.293-2.31,2.292,1.034,2.292,2.31-1.026,2.31-2.292,2.31Zm16.485,14.158h-3.858v-6.571c0-1.802-.685-2.809-2.111-2.809-1.551,0-2.362,1.048-2.362,2.809v6.571h-3.718V12.87h3.718v1.686s1.118-2.069,3.775-2.069,4.556,1.621,4.556,4.975v7.926Z"
+										/>
+									</svg>
+									LinkedIn
+								</a>
+							</li>
+							<li>
+								<a>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										className="h-7 w-7"
+										fill="none"
+										viewBox="0 0 32 32"
+										stroke="currentColor"
+									>
+										<path
+											// strokeLinecap="round"
+											// strokeLinejoin="round"
+											// strokeWidth="2"
+											d="M16,2.345c7.735,0,14,6.265,14,14-.002,6.015-3.839,11.359-9.537,13.282-.7,.14-.963-.298-.963-.665,0-.473,.018-1.978,.018-3.85,0-1.312-.437-2.152-.945-2.59,3.115-.35,6.388-1.54,6.388-6.912,0-1.54-.543-2.783-1.435-3.762,.14-.35,.63-1.785-.14-3.71,0,0-1.173-.385-3.85,1.435-1.12-.315-2.31-.472-3.5-.472s-2.38,.157-3.5,.472c-2.677-1.802-3.85-1.435-3.85-1.435-.77,1.925-.28,3.36-.14,3.71-.892,.98-1.435,2.24-1.435,3.762,0,5.355,3.255,6.563,6.37,6.913-.403,.35-.77,.963-.893,1.872-.805,.368-2.818,.963-4.077-1.155-.263-.42-1.05-1.452-2.152-1.435-1.173,.018-.472,.665,.017,.927,.595,.332,1.277,1.575,1.435,1.978,.28,.787,1.19,2.293,4.707,1.645,0,1.173,.018,2.275,.018,2.607,0,.368-.263,.787-.963,.665-5.719-1.904-9.576-7.255-9.573-13.283,0-7.735,6.265-14,14-14Z"
+										/>
+									</svg>
+									GitHub
+								</a>
+							</li>
+							<li>
+								<a>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										className="h-7 w-7"
+										fill="none"
+										viewBox="0 0 32 32"
+										stroke="currentColor"
+									>
+										<path
+											// strokeLinecap="round"
+											// strokeLinejoin="round"
+											// strokeWidth="2"
+											d="M29 4H3a3 3 0 0 0-3 3v18a3 3 0 0 0 3 3h26a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-.72 2L16 14.77 3.72 6zM30 25a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.23l13.42 9.58a1 1 0 0 0 1.16 0L30 7.23z"
+										/>
+									</svg>
+									E-mail
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<Timeline />
+		</div>
+	);
 }
