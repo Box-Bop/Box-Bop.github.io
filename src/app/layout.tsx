b17d622738079eaf76c4c7cadd59459eaf0b504e
BoxBop <tohverrobert@gmail.com>
2025-02-06 14:23:10 +0200

    Added a simple GoatCounter analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,37 +3,42 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Roberts portfolio",
-  description: ""
+	title: "Roberts portfolio",
+	description: "",
 };
 
 export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-}
+	width: "device-width",
+	initialScale: 1,
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html data-theme="dark" lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html data-theme="dark" lang="en">
+			<script
+				data-goatcounter="https://statisticdictationlevitateobtuseimmersion.goatcounter.com/count"
+				async
+				src="//gc.zgo.at/count.js"
+			></script>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
